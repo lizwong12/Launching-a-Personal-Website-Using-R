8c667bab2c212ef150a2c6bc78ba5d0d391c4eea
lizwong12 <153999125+lizwong12@users.noreply.github.com>
2023-12-16 08:54:11 -0500

    Create push_to_git.sh

---
+++ push_to_git.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# add all files to the local Git repository
+git add --all
+
+# commit files and include a brief message
+git commit -m "brief message describing your changes"
+
+# push files to GitHub
+git push origin master
